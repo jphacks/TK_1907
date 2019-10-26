72b8f77b396b1d9da0713f689357a1548b0d284a
u-sho <720187sho@gmail.comm>
2019-10-26 12:45:11 +0900

    fixed store module syntax

--- Web/store/index.js
+++ Web/store/index.js
@@ -1,26 +1,20 @@
-import Vuex from 'vuex'
+export const state = () => ({
+  scriptHash: "aa0631bd09b916affa8188a48df04f7f426aca29",
+  privateKey: "",
+  isLogin: false,
+  neoGas: "0",
+  comics: []
+});
 
-const store = () => new Vuex.Store({
-
-  state: {
-    scriptHash: 'aa0631bd09b916affa8188a48df04f7f426aca29',
-    privateKey: '',
-    isLogin: false,
-    neoGas: '0',
-    comics: []
+export const mutations = {
+  login(state, privateKey) {
+    state.privateKey = privateKey;
+    state.isLogin = true;
   },
-  mutations: {
-    login (state, privateKey) {
-      state.privateKey = privateKey
-      state.isLogin = true
-    },
-    setNeoGas(state, neoGas) {
-      state.neoGas = neoGas
-    },
-    setComics(state, comics) {
-      state.comics = comics
-    }
+  setNeoGas(state, neoGas) {
+    state.neoGas = neoGas;
+  },
+  setComics(state, comics) {
+    state.comics = comics;
   }
-})
-
-export default store
+};
