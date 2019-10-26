b78f7996ad404cd39152605a6be1f7cf74b4fdd3
imagine <imajinkawabe@gmail.com>
2019-10-26 12:10:18 +0900

    feat: Set up base of HomeViewReactor

--- iOS/Marine/UI/Home/HomeViewReactor.swift
+++ iOS/Marine/UI/Home/HomeViewReactor.swift
@@ -7,3 +7,70 @@
 //
 
 import Foundation
+import RxSwift
+import ReactorKit
+import FirebaseFirestore
+import Swinject
+import SwinjectAutoregistration
+
+final class HomeViewReactor: Reactor {
+
+    var initialState: State
+    let bookService: BookService
+
+    enum Action {
+        case load
+        case select
+        case loadNext
+    }
+
+    enum Mutation {
+        case setBooks([Book], lastSnapshot: DocumentSnapshot?, hasNext: Bool)
+        case appendBooks([Book], lastSnapshot: DocumentSnapshot?, hasNext: Bool)
+        case setLoading(isLoading: Bool)
+        case setFirstLoading(isLoading: Bool)
+    }
+
+    struct State {
+        var isFirstLoading: Bool
+        var isLoading: Bool
+        var headerSection: CustomSection
+        var booksSection: CustomSection
+        var hasNext: Bool
+        var lastSnapshot: DocumentSnapshot?
+    }
+
+    init() {
+        initialState = State(isFirstLoading: false, isLoading: false, headerSection: .init(model: .homeHeader, items: []), booksSection: .init(model: .books, items: []), hasNext: true, lastSnapshot: nil)
+        self.bookService = BookService()
+    }
+
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .load:
+            return .concat([
+                .just(.setFirstLoading(isLoading: true)),
+                bookService.fetch().map { result in
+                    .setBooks(result.books, lastSnapshot: result.lastSnapshot, hasNext: result.hasNext)
+                },
+                .just(.setFirstLoading(isLoading: false))
+                ])
+        default:
+            return .empty()
+        }
+    }
+
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case let .setBooks(books, lastSnapshot: lastSnapshot, hasNext: hasNext):
+            newState.booksSection.items = books.map(CellItem.book)
+            newState.lastSnapshot = lastSnapshot
+            newState.hasNext = hasNext
+        default:
+            break;
+        }
+        return newState
+    }
+}
+
