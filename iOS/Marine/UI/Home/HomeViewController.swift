49b7e52d4045a5d52cb9b9da90389652a5662f5b
imagine <imajinkawabe@gmail.com>
2019-10-26 11:36:04 +0900

    fix: Removed TapticEngine

--- iOS/Marine/UI/Home/HomeViewController.swift
+++ iOS/Marine/UI/Home/HomeViewController.swift
@@ -6,6 +6,9 @@
 //  Copyright © 2019 Imagine Kawabe. All rights reserved.
 //
 
+import ReactorKit
+import RxDataSource
+import RxSwift
 import UIKit
 
 class HomeViewController: UIViewController {
@@ -15,16 +18,4 @@ class HomeViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- iOS/Pods/Target Support Files/TapticEngine/TapticEngine-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TapticEngine : NSObject
-@end
-@implementation PodsDummy_TapticEngine
-@end
