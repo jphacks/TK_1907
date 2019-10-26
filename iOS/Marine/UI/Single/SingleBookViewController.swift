c5c16abcb9b02ee7d2fc48494e51bab0d7e118b8
imagine <imajinkawabe@gmail.com>
2019-10-26 18:34:18 +0900

    feat: Set up single page and changed home header image

---
+++ iOS/Marine/UI/Single/SingleBookViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SingleBookViewController.swift
+//  Marine
+//
+//  Created by Imagine Kawabe on 2019/10/26.
+//  Copyright © 2019 Imagine Kawabe. All rights reserved.
+//
+
+import UIKit
+
+final class SingleBookViewController: UIViewController {
+
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var bookTitleLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
