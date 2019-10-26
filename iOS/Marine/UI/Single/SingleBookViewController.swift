1ca70665aec65964f635de1a0b5cdbb2ff0af506
imagine <imajinkawabe@gmail.com>
2019-10-26 19:18:56 +0900

    feat: Updated Single VC

--- iOS/Marine/UI/Single/SingleBookViewController.swift
+++ iOS/Marine/UI/Single/SingleBookViewController.swift
@@ -12,10 +12,12 @@ import RxCocoa
 import RxDataSources
 import RxSwift
 import SwinjectStoryboard
+import WaterfallLayout
 
 final class SingleBookViewController: UIViewController, StoryboardInstantiate {
 
     static var storyboardName: StoryboardName = .single
+    var disposeBag: DisposeBag = DisposeBag()
 
     @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet weak var thumbnailImageView: UIImageView!
@@ -25,5 +27,21 @@ final class SingleBookViewController: UIViewController, StoryboardInstantiate {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+}
+
+extension SingleBookViewController: StoryboardView {
+    func bind(reactor: SingleBookViewReactor) {
+
+    }
+}
+
+extension SingleBookViewController: UICollectionViewDelegate {
+    func collectionViewLayout(for section: Int) -> WaterfallLayout.Layout {
+        return .flow(column: 1)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout: WaterfallLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width / 3.5)
     }
 }
