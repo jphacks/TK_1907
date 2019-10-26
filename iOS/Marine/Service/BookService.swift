fc57396e45cabc29d3b54cbdad5ca8eb2d55d487
imagine <imajinkawabe@gmail.com>
2019-10-26 11:56:27 +0900

    feat: Basic of BookService

---
+++ iOS/Marine/Service/BookService.swift
@@ -0,0 +1,25 @@
+//
+//  ItemService.swift
+//  Marine
+//
+//  Created by Imagine Kawabe on 2019/10/26.
+//  Copyright © 2019 Imagine Kawabe. All rights reserved.
+//
+
+import Foundation
+import FirebaseFirestore
+import RxSwift
+import RxFirebase
+
+final class BookService {
+
+    static let booksPerPage = 10
+
+    func fetch() -> Observable<ItemQueryResult> {
+        let baseRef =  Firestore.firestore().collection("Books")
+        return baseRef.limit(to: BookService.booksPerPage).rx.getDocuments()
+        .map({ querySnapshot in
+            return ItemQueryResult(querySnapshot: querySnapshot)
+        })
+    }
+}
