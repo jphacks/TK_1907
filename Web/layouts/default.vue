dac0c936895ee24d2c8eba74b01c22dc817a3edd
u-sho <720187sho@gmail.comm>
2019-10-26 23:22:13 +0900

    extracted Footer

--- Web/layouts/default.vue
+++ Web/layouts/default.vue
@@ -2,13 +2,15 @@
   <div>
     <Header />
     <nuxt />
+    <Footer />
   </div>
 </template>
 
 <script>
 export default {
   components: {
-    Header: () => import("@/components/Header.vue")
+    Header: () => import("~/components/Header.vue"),
+    Footer: () => import("~/components/Footer.vue")
   }
 };
 </script>
