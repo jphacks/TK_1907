0517c35668ca3ee9e905c39e2a3da43626fe654a
imagine <imajinkawabe@gmail.com>
2019-10-26 12:21:40 +0900

    feat: Added files for Home cells

---
+++ iOS/Marine/UI/Components/BookCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  BookCollectionViewCell.swift
+//  Marine
+//
+//  Created by Imagine Kawabe on 2019/10/26.
+//  Copyright © 2019 Imagine Kawabe. All rights reserved.
+//
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ iOS/Marine/UI/Components/HomeHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeHeaderView.swift
+//  Marine
+//
+//  Created by Imagine Kawabe on 2019/10/26.
+//  Copyright © 2019 Imagine Kawabe. All rights reserved.
+//
+
+import UIKit
+
+class HomeHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
