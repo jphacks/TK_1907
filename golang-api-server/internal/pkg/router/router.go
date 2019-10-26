7fe12449c4eae744c8b4d1165dce9e7ede196e19
nandehu0323 <hiroki.nanjo@ginco.io>
2019-10-27 05:30:04 +0900

    :wrench: Fix: add CORS middleware

--- golang-api-server/internal/pkg/router/router.go
+++ golang-api-server/internal/pkg/router/router.go
@@ -6,11 +6,14 @@ import (
 	"github.com/jphacks/TK_1907/golang-api-server/internal/pkg/handler"
 	"github.com/jphacks/TK_1907/golang-api-server/internal/pkg/storage"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 // Init ...
 func Init(db database.Database, s storage.Storage, eth ethereum.Ethereum) *echo.Echo {
 	e := echo.New()
+
+	e.Use(middleware.CORS())
 
 	e.GET("/health", handler.HealthCheck())
 	e.GET("/getImage/:addr/:chapter/:page", handler.GetImage(db, s))
